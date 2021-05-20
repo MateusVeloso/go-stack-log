fc0270703efa3c6eea2a7f0411241f66db3abdb0
Mateus Veloso <mateusveloso.developer@gmail.com>
2021-05-20 01:49:21 -0300

    Submit repo

---
+++ entity/track.go
@@ -0,0 +1,8 @@
+package entity
+
+//Track data
+type Track struct {
+	ID          ID
+	Name        string
+	Description string
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mateusveloso/go-track-log
+
+go 1.16
+
+require github.com/google/uuid v1.2.0
